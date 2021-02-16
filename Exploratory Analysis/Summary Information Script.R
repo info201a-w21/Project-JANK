9d088bef251db0c6c85f4c83933a6ece99633467
Angela He <bbangela26@Angelas-MacBook-Pro.local>
2021-02-16 11:15:55 -0800

    summary script edit

---
+++ Exploratory Analysis/Summary Information Script.R
@@ -0,0 +1,18 @@
+## Load Data
+poverty.data <- read.csv('covid_data_log_200922.csv') 
+
+## Directions:
+# A file that calculates summary information to be included in your report
+# (e.g nrow, ncol, max, min, avg, median, etc.)
+
+# Nrow
+
+# Ncol
+
+# Max
+
+# Min
+
+# Avg
+
+# Median

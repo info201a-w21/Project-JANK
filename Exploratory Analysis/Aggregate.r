1309493bc4ca6b49f47f6025594d836d2d98816a
er9966 <justinagao9@gmail.com>
2021-02-16 14:27:38 -0800

    uploaded new data, made changes to aggregate

--- Exploratory Analysis/Aggregate.r
+++ Exploratory Analysis/Aggregate.r
@@ -1,1 +1,15 @@
 poverty_data <- read.csv('covid_data_log_200922.csv')
+
+# This table contains the country that has the most number of individuals
+# living in poverty from each state, number of covid cases and deaths in 
+# the county.
+
+library(tidyverse)
+
+most_poverty_cases_deaths_log <- poverty_data %>% 
+  group_by(State) %>% 
+  filter(Poverty == max(Poverty, na.rm = T)) %>% 
+  select(County, State, Poverty, Cases, Deaths)
+
+
+  

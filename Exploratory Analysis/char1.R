9d969f63529acf586edbf5985e7a946a2b28f4a3
er9966 <justinagao9@gmail.com>
2021-02-17 09:55:12 -0800

    created a chart of sum of poor exemptions over time

---
+++ Exploratory Analysis/char1.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+poverty_data <- read.csv('irs.csv')
+
+# find the change in number of poor exemptions in each state over year
+
+num_poverty_data <- poverty_data %>% 
+  transmute(num_poor.exemptions = as.numeric(gsub(",","", Poor.exemptions)),
+            year = as.numeric(Year), state = Name) %>% 
+  group_by(year) %>% 
+  summarise(total_poor_exemptions = sum(num_poor.exemptions))
+
+poor_exemption_over_year <- ggplot(num_poverty_data) +
+  geom_line(mapping = aes(x = year, y = total_poor_exemptions)) +
+  labs(x = "Year", y = "Number of Poor Exemtions in U.S.",
+       title = "Change in Total Number of Poor Exemption Over Time in U.S.") +
+  scale_x_continuous("year", labels = as.character(num_poverty_data$year), 
+                     breaks = num_poverty_data$year) +
+  theme(axis.text.x = element_text(angle = 65))

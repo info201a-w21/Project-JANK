6524731f102d91f5787fa0058708400fd3c0f3be
Angela He <bbangela26@Angelas-MacBook-Pro.local>
2021-02-17 15:54:13 -0800

    script second edit

--- Exploratory Analysis/Summary Information Script.R
+++ Exploratory Analysis/Summary Information Script.R
@@ -1,18 +1,24 @@
 ## Load Data
-poverty.data <- read.csv('covid_data_log_200922.csv') 
+poverty.data <- read.csv('covid_data_log_200922.csv')
+irs.data <- read.csv('irs.csv')
+
+## Covert Data
+most_exemption_cases_deaths <- irs.data %>% 
+  group_by(Name) %>% 
+  filter(Poor.exemptions == max(Poor.exemptions, na.rm = T)) %>% 
+  select(Name, Year, Total.exemptions, Poor.exemptions, Mean.AGI)
 
 ## Directions:
 # A file that calculates summary information to be included in your report
 # (e.g nrow, ncol, max, min, avg, median, etc.)
-
-# Nrow
-
-# Ncol
 
 # Max
 
 # Min
 
 # Avg
+avg.poor.exemption <- irs.data %>% 
+  group_by(Year) %>% 
+  summarize(Poor.exemptions == mean(Poor.exemptions, na.rm = T))
 
 # Median

91bc0058e04835ca097572808bfa502bddcffe89
nfang2001 <nicolefang2001@gmail.com>
2021-02-19 00:07:57 -0800

    added scatter plot for most and least poorest states

---
+++ Exploratory Analysis/char3.R
@@ -0,0 +1,26 @@
+## Load Data
+library(tidyverse)
+irs_data <- read.csv('irs.csv') 
+
+# Find the mean and median AGI number of the U.S. state with the most and least poverty
+
+
+Poverty <- irs_data %>% 
+  group_by(Year, Name) %>%
+  mutate(Mean_AGI = as.numeric(gsub(",","", Mean.AGI)),
+         Median_AGI = as.numeric(gsub(",","", Median.AGI))) %>% 
+  select(Year, Name, Mean_AGI, Median_AGI)
+
+
+compare_poverty_states <-  Poverty %>% 
+  filter(Name == "Connecticut" | Name == "Mississippi")
+
+
+chart3 <- ggplot(compare_poverty_states)+
+  geom_point(mapping = aes(x = Mean_AGI, y = Median_AGI, color = Name))+
+  labs(title = "Mean and median AGI comparison between the U.S. state with most and least poverty ",
+       subtitle = "From 1990 - 2018",
+       x = "Mean AGI",
+       y = "Median AGI")
+
+print(chart3)

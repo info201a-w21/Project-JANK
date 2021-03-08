e431fe7216e3b8e6ad161162d440a7fc83e84233
kennkho <kennkho@uw.edu>
2021-03-08 14:29:54 -0800

    base lay

--- final/app_ui.R
+++ final/app_ui.R
@@ -1,10 +1,10 @@
 ui <- fluidPage(theme = shinytheme("yeti"),
                 navbarPage("Poverty and Income Inequality"#,
-                           # intro,
-                           # page_one,
-                           # page_two,
-                           # page_three,
-                           # conclusion 
+                           # intro_page,
+                           # viz_one,
+                           # viz_two,
+                           # viz_three,
+                           # conclusion_page 
                            )
 
                 )

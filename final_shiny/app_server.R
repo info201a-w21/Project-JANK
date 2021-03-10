7ade2487fd1edc2a32ab9efe425f84ac70e266c2
kennkho <kennkho@uw.edu>
2021-03-10 15:41:43 -0800

    not complete testing viz1

--- final_shiny/app_server.R
+++ final_shiny/app_server.R
@@ -1,12 +1,26 @@
 poverty_data <- read.csv('irs.csv')
 
+mutated_data <- poverty_data %>% 
+  transmute(Number_Total_exemptions = as.numeric(gsub(",","", Total.exemptions)),
+         Number_Poor_exemptions = as.numeric(gsub(",","", Poor.exemptions)),
+         Number_65_over_exemptions = as.numeric(gsub(",","", Age.65.and.over.poor.exemptions)),
+         Number_65_under_exemptions = as.numeric(gsub(",","", Poor.exemptions.under.age.65)),
+         Number_Poor_Child_exemptions = as.numeric(gsub(",","", Poor.child.exemptions)),
+         Year = as.numeric(Year), 
+         State = Name) %>% 
+  select(State, Year, Number_Total_exemptions, Number_Poor_exemptions,
+         Number_65_over_exemptions, Number_65_under_exemptions,
+         Number_Poor_Child_exemptions)
 
-
-
-
-
-
-
+# Filter for avg exemptions each year
+Avg_exemptions <- mutated_data %>% 
+  group_by(Year) %>% 
+  summarise(Number_Total_exemptions = mean(sum(Number_Total_exemptions)),
+            Number_Poor_exemptions = mean(sum(Number_Poor_exemptions)),
+            Number_65_over_exemptions = mean(sum(Number_65_over_exemptions)),
+            Number_65_under_exemptions = mean(sum(Number_65_under_exemptions)),
+            Number_Poor_Child_exemptions = mean(sum(Number_Poor_Child_exemptions)))
+      
 
 
 
@@ -14,5 +28,17 @@ poverty_data <- read.csv('irs.csv')
 
 
 server <- function(input, output){
+  
+  output$Viz1 <- renderPlotly({
+    
+    filtered_data <- Avg_exemptions %>%
+      filter(Year >= "1989" & Year <= "2018")
+      # filter(State == input$state)
+    
+    plot1 <- ggplot(data = filtered_data)+
+      geom_line(mapping = aes_string(x = "Year", y = input$exemptype))
+ 
+    ggplotly(plot1)
+       })
   
 }
--- final_shiny/app_ui.R
+++ final_shiny/app_ui.R
@@ -2,15 +2,38 @@ intro_page <- tabPanel(
   "Introduction"
 )
 
+
+mutated_data <- poverty_data %>% 
+  transmute(Number_Total_exemptions = as.numeric(gsub(",","", Total.exemptions)),
+            Number_Poor_exemptions = as.numeric(gsub(",","", Poor.exemptions)),
+            Number_65_over_exemptions = as.numeric(gsub(",","", Age.65.and.over.poor.exemptions)),
+            Number_65_under_exemptions = as.numeric(gsub(",","", Poor.exemptions.under.age.65)),
+            Number_Poor_Child_exemptions = as.numeric(gsub(",","", Poor.child.exemptions)),
+            Year = as.numeric(Year), 
+            State = Name) %>% 
+  select(State, Year, Number_Total_exemptions, Number_Poor_exemptions,
+         Number_65_over_exemptions, Number_65_under_exemptions,
+         Number_Poor_Child_exemptions)
+
+filtered_states <- unique(mutated_data$State)
+
 viz_one <- tabPanel(
   "Visualization 1",
-  titlePanel("gfgf")
-)
+  titlePanel("Trend In Exemptions Over Time"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "state", label = "Select State",
+                  choices = filtered_states, selected = "Washington"),
+      
+      radioButtons(inputId = "exemptype", label = ("Choose Exemption To View"),
+                   choices = list("Number_Total_exemptions", "Number_Poor_exemptions","Number_65_over_exemptions",
+                                  "Number_65_under_exemptions","Number_Poor_Child_exemptions"),
+                   selected = "Number_Poor_Child_exemptions")),
 
-
-
-
-
+    mainPanel(
+      plotlyOutput(outputId = "Viz1")
+    )
+))
 
 
 
